daa52d51564ab06f98caf042225308878f0c3556
RobinSardja <robinsardja@gmail.com>
2024-09-20 17:14:22 -0400

    Built basic layout.

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,58 @@ void main() {
     runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
     const MainApp({super.key});
 
     @override
+    State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+
+    int currIndex = 1;
+    final pageController = PageController( initialPage: 1 );
+
+    @override
     Widget build(BuildContext context) {
-        return const MaterialApp(
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
             home: Scaffold(
-                body: Center(
-                    child: Text("Hello World!"),
+                appBar: AppBar(
+                    centerTitle: true,
+                    title: const Text( "SnackChat" )
                 ),
-            ),
+                body: PageView(
+                    controller: pageController,
+                    onPageChanged: (currPage) => setState( () => currIndex = currPage ),
+                    children: const [
+                        Center( child: Text( "Home" ) ),
+                        Center( child: Text( "Explore" ) ),
+                        Center( child: Text( "Settings" ) )
+                    ]
+                ),
+                bottomNavigationBar: BottomNavigationBar(
+                    currentIndex: currIndex,
+                    items: const [
+                        BottomNavigationBarItem(
+                            icon: Icon( Icons.home ),
+                            label: "Home"
+                        ),
+                        BottomNavigationBarItem(
+                            icon: Icon( Icons.explore ),
+                            label: "Explore"
+                        ),
+                        BottomNavigationBarItem(
+                            icon: Icon( Icons.settings ),
+                            label: "Settings"
+                        )
+                    ],
+                    onTap: (selectedIndex) {
+                        setState( () => currIndex = selectedIndex );
+                        pageController.jumpToPage( currIndex );
+                    }
+                )
+            )
         );
     }
 }
